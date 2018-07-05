30a523a26ed82917ba345fedca426e8e469e53cf
CORPORATE\jmd0312 <jacob.douglas@daugherty.com>
2018-07-05 14:15:22 -0500

    Added Calculator Class Started int methods.

---
+++ src/Calculator.java
@@ -0,0 +1,22 @@
+
+public class Calculator {
+	public int add(int x, int y) {
+		//TODO: Complete method.
+		return 0;
+	}
+	
+	public int subtract(int x, int y) {
+		//TODO: Complete method.
+		return 0;
+	}
+	
+	public int divide(int x, int y) {
+		//TODO: Complete method.
+		return 0;
+	}
+	
+	public int multiply(int x, int y) {
+		//TODO: Complete method
+		return 0;
+	}
+}
