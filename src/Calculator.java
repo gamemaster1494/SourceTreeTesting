c109f5edcf6ecc20dd047b3744ccac80b5f1ff3e
CORPORATE\jmd0312 <jacob.douglas@daugherty.com>
2018-07-05 14:53:13 -0500

    Added the methods for Double types.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,6 @@
 
 public class Calculator {
+	
 	public int add(int x, int y) {
 		//TODO: Complete method.
 		return 0;
@@ -16,6 +17,27 @@ public class Calculator {
 	}
 	
 	public int multiply(int x, int y) {
+		//TODO: Complete method
+		return 0;
+	}
+
+
+	public double add(double x, double y) {
+		//TODO: Complete method
+		return 0;
+	}
+	
+	public double subtract(double x, double y) {
+		//TODO: Complete method
+		return 0;
+	}
+	
+	public double divide(double x, double y) {
+		//TODO: Complete method
+		return 0;
+	}
+	
+	public double multiply(double x, double y) {
 		//TODO: Complete method
 		return 0;
 	}
